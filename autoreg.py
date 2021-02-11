469891fe0cefa30fdf11b6ae4f12843a22f6c209
KajimaSoys <43302420+KajimaSoys@users.noreply.github.com>
2021-02-11 13:53:39 +0300

    v.2.2

--- autoreg.py
+++ autoreg.py
@@ -25,6 +25,9 @@ print(surdb[surindex]+' '+namedb[nameindex])
 fmn.close()
 fs.close()
 
+day=random.randint(0,5)
+
+
 #подключаем вебдрайвер
 options = webdriver.ChromeOptions() 
 options.add_argument("download.default_directory=D:\Учеба\АИС\torgi_parser_Efim")
@@ -39,7 +42,21 @@ nameform.send_keys(namedb[nameindex])
 surform = driver.find_element_by_xpath('//*[@id="lname"]')
 surform.click()
 surform.send_keys(surdb[surindex])
+time.sleep(3)
+dselect = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/form/div[5]/div[2]/div/div[1]/div/div')
+dselect.click()
+#dselect = Select(driver.find_element_by_class_name('select-0-2-241'))                                               
+#dselect.select_by_value('3')
+time.sleep(10)
+dselect = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/form/div[5]/div[2]/div/div[1]/div/div')
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/form/div[5]/div[2]/div/div[1]/div/div').send_keys(Keys.DOWN)
+driver.send_keys(Keys.DOWN).perform()
+#dselect.click()
 
+#for i in range(day):
+#    dselect.send_keys(Keys.ARROW_DOWN).perform()
+#    time.sleep(1)
+#dselect.send_keys(Keys.RETURN)
 
 time.sleep(200)
 driver.quit()
